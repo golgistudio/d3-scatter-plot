62f48221d18a3047929dc4efe46c5c1dd811d856
Laurie Reynolds <LaurieSReynolds@gmail.com>
2015-11-04 18:14:46 -0800

    add fail on error
    Trying to get Travis CI build to fail if any warnings or errors from jshint run

--- gulpfile.js/tasks/jshint.js
+++ gulpfile.js/tasks/jshint.js
@@ -8,6 +8,8 @@ var jshint       = require('gulp-jshint');
 var stylish       = require('jshint-stylish');
 var handleErrors = require('../lib/handleErrors');
 
+var notify = require('gulp-notify');
+
 var paths = {
     src: path.join(config.root.src, config.tasks.jshint.src, '/**/*.{' + config.tasks.jshint.extensions + '}'),
     propFile: path.join(config.root.src, config.tasks.jshint.props)
@@ -15,10 +17,8 @@ var paths = {
 
 var jshintTask = function () {
     return gulp.src(paths.src)
-        .pipe(jshint(paths.propFile))
-        .on('error', handleErrors)
+        .pipe(jshint(paths.propFile, {fail: true}))
         .pipe(jshint.reporter(stylish))
-        .on('error', handleErrors)
         .pipe(jshint.reporter('fail'))
         .on('error', handleErrors);
 };
