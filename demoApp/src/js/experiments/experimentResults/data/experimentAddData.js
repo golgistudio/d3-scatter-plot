67311844635650f8c3fdd46116d625275f91f9c5
golgistudio <lauriesreynolds@gmail.com>
2015-11-05 09:26:35 -0800

    change name from Subjects to Subject (singular)

--- demoApp/src/js/experiments/experimentResults/data/experimentAddData.js
+++ demoApp/src/js/experiments/experimentResults/data/experimentAddData.js
@@ -8,143 +8,143 @@
  */
 var experimentAddData = [
     {
-        "Subjects": "P-100",
+        "Subject": "P-100",
         "Control":     12.079,
         "Test":        19.278,
         "Difference":  7.199
     }, {
-        "Subjects": "P-101",
+        "Subject": "P-101",
         "Control":     16.791,
         "Test":        18.741,
         "Difference":  1.95
     }, {
-        "Subjects": "P-102",
+        "Subject": "P-102",
         "Control":     9.564,
         "Test":        21.214,
         "Difference":  11.65
     }, {
-        "Subjects": "P-103",
+        "Subject": "P-103",
         "Control":     8.63,
         "Test":        15.687,
         "Difference":  7.057
     }, {
-        "Subjects": "P-104",
+        "Subject": "P-104",
         "Control":     14.669,
         "Test":        22.803,
         "Difference":  8.134
     }, {
-        "Subjects": "P-105",
+        "Subject": "P-105",
         "Control":     12.238,
         "Test":        20.878,
         "Difference":  8.64
     }, {
-        "Subjects": "P-106",
+        "Subject": "P-106",
         "Control":     14.692,
         "Test":        24.572,
         "Difference":  9.88
     }, {
-        "Subjects": "P-107",
+        "Subject": "P-107",
         "Control":     8.987,
         "Test":        17.394,
         "Difference":  8.407
     }, {
-        "Subjects": "P-108",
+        "Subject": "P-108",
         "Control":     9.401,
         "Test":        20.762,
         "Difference":  11.361
     }, {
-        "Subjects": "P-109",
+        "Subject": "P-109",
         "Control":     14.48,
         "Test":        26.282,
         "Difference":  11.802
     }, {
-        "Subjects": "P-110",
+        "Subject": "P-110",
         "Control":     22.328,
         "Test":        24.524,
         "Difference":  2.196
     }, {
-        "Subjects": "P-111",
+        "Subject": "P-111",
         "Control":     15.298,
         "Test":        18.644,
         "Difference":  3.346
     }, {
-        "Subjects": "P-112",
+        "Subject": "P-112",
         "Control":     15.073,
         "Test":        17.51,
         "Difference":  2.437
     }, {
-        "Subjects": "P-113",
+        "Subject": "P-113",
         "Control":     16.929,
         "Test":        20.33,
         "Difference":  3.401
     }, {
-        "Subjects": "P-114",
+        "Subject": "P-114",
         "Control":     18.2,
         "Test":        35.255,
         "Difference":  17.055
     }, {
-        "Subjects": "P-115",
+        "Subject": "P-115",
         "Control":     12.13,
         "Test":        22.158,
         "Difference":  10.028
     }, {
-        "Subjects": "P-116",
+        "Subject": "P-116",
         "Control":     18.495,
         "Test":        25.139,
         "Difference":  6.644
     }, {
-        "Subjects": "P-117",
+        "Subject": "P-117",
         "Control":     10.639,
         "Test":        20.429,
         "Difference":  9.79
     }, {
-        "Subjects": "P-118",
+        "Subject": "P-118",
         "Control":     11.344,
         "Test":        17.425,
         "Difference":  6.081
     }, {
-        "Subjects": "P-119",
+        "Subject": "P-119",
         "Control":     12.369,
         "Test":        34.288,
         "Difference":  21.919
     }, {
-        "Subjects": "P-120",
+        "Subject": "P-120",
         "Control":     12.944,
         "Test":        23.894,
         "Difference":  10.95
     }, {
-        "Subjects": "P-121",
+        "Subject": "P-121",
         "Control":     14.233,
         "Test":        17.96,
         "Difference":  3.727
     }, {
-        "Subjects": "P-122",
+        "Subject": "P-122",
         "Control":     19.71,
         "Test":        22.058,
         "Difference":  2.348
     }, {
-        "Subjects": "P-123",
+        "Subject": "P-123",
         "Control":     16.004,
         "Test":        21.157,
         "Difference":  5.153
     },
     {
-        "Subjects": "P-124",
+        "Subject": "P-124",
         "Control":     23.944,
         "Test":        58.894,
         "Difference":  13.95
     }, {
-        "Subjects": "P-125",
+        "Subject": "P-125",
         "Control":     24.233,
         "Test":        55.96,
         "Difference":  6.727
     }, {
-        "Subjects": "P-126",
+        "Subject": "P-126",
         "Control":     29.71,
         "Test":        22.058,
         "Difference":  5.348
     }, {
-        "Subjects": "P-127",
+        "Subject": "P-127",
         "Control":     26.004,
         "Test":        60.157,
         "Difference":  8.153
